332429d4b44fd0ae9d19c9ed560412da2eb76108
jaeseong <wotjd0701@naver.com>
2025-07-12 20:45:52 +0900

    gpt_services 추가

---
+++ backend/services/gpt_service.py
@@ -0,0 +1,23 @@
+def summarize_text(text):
+    """
+    GPT API를 연동해 입력된 텍스트 요약 생성
+    현재는 더미 요약을 반환함
+    """
+    return 
+    # TODO 
+
+def chat_with_context(question):
+    """
+    GPT API를 연동해 단순 질문에 응답
+    현재는 더미 응답을 반환함
+    """
+    return 
+    # TODO 
+
+def answer_question(context, question):
+    """
+    TODO: GPT API를 연동해 문맥 + 질문으로 응답 생성
+    현재는 더미 응답을 반환함
+    """
+    return 
+    # TODO 
