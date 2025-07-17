57dc864e3dc4ba6a50592b8cf40ea27886ee219d
jaeseong <wotjd0701@naver.com>
2025-07-17 17:24:41 +0900

    services 도메인 전체 수정

--- backend/services/gpt_service.py
+++ backend/services/gpt_service.py
@@ -1,20 +1,3 @@
-def summarize_text(text):
-    """
-    입력된 텍스트 요약 생성
-    """
-    return 
-    # TODO 
-
-def chat_with_context(question):
-    """
-    질문에 응답
-    """
-    return 
-    # TODO 
-
-def answer_question(context, question):
-    """
-     응답 생성
-    """
-    return 
-    # TODO 현재 더미데이터, 이후 질문, 요약, 응답 함수 구현
+def chat_with_context(user_text: str) -> str:
+    # TODO: 
+    return f"'{user_text}'에 대한 응답입니다."
