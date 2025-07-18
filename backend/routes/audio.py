1d428826861b3d4a8a339186870dc9540e2c976f
jaeseong <wotjd0701@naver.com>
2025-07-18 10:34:21 +0900

    routes 삭제

--- backend/routes/audio.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, request
-from services.audio_service import process_audio
-
-audio_bp = Blueprint('audio', __name__)
-
-@audio_bp.route("/process", methods=["POST"])
-def handle_process():
-    return process_audio(request)
