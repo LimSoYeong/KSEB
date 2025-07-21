57fe47ff7aa167b770bf6c8f48aa4e9e46999056
jaeseong <wotjd0701@naver.com>
2025-07-21 20:48:41 +0900

    이미지->텍스트->요약->tts 도메인

---
+++ backend/domain/summarize_entity.py
@@ -0,0 +1,5 @@
+class SummaryResult:
+    def __init__(self, original_text: str, summary_text: str, audio_path: str):
+        self.original_text = original_text
+        self.summary_text = summary_text
+        self.audio_path = audio_path
