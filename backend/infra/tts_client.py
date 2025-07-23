e92676d4d3bf6a617bc94265751e05beb494bd6e
jaeseong <wotjd0701@naver.com>
2025-07-23 14:52:05 +0900

    tts API코드

--- backend/infra/tts_client.py
+++ backend/infra/tts_client.py
@@ -6,7 +6,23 @@ from config.settings import TTS_API_KEY
 def text_to_speech(text: str) -> str:
     filename = f"{uuid.uuid4().hex}.mp3"
     path = Path("static/audio") / filename
-    with open(path, "wb") as f:
-        f.write(b"DUMMY MP3 DATA")  
-        # TODO: TTS API, 현재는 더미
-    return str(path)
+
+    url = ""
+    headers = {
+        "X-NCP-APIGW-API-KEY-ID": os.getenv("NAVER_CLIENT_ID"),
+        "X-NCP-APIGW-API-KEY": TTS_API_KEY,
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    data = {
+        "speaker": "nara",
+        "speed": "0",
+        "text": text
+    }
+
+    response = requests.post(url, headers=headers, data=data)
+    if response.status_code == 200:
+        with open(path, "wb") as f:
+            f.write(response.content)
+        return str(path)
+    else:
+        raise RuntimeError(f"TTS 요청 실패: {response.status_code} - {response.text}")
