5b3155c43ef7dfc9334abc2e0d0dc72f7f801484
jaeseong <wotjd0701@naver.com>
2025-07-23 15:18:09 +0900

    stt API연동 기능 구현

--- backend/infra/stt_client.py
+++ backend/infra/stt_client.py
@@ -1,20 +1,19 @@
 import requests
+from config.settings import STT_API_KEY
 
 def speech_to_text(audio_path: str) -> str:
-    url = "https"
+    url = ""
     headers = {
-        "X-NCP-APIGW-API-KEY-ID": "",
-        "X-NCP-APIGW-API-KEY": "",
+        "X-NCP-APIGW-API-KEY-ID": os.getenv("NAVER_CLIENT_ID"),
+        "X-NCP-APIGW-API-KEY": STT_API_KEY,
         "Content-Type": "application/octet-stream"
     }
-
     with open(audio_path, "rb") as f:
         audio_data = f.read()
 
-    response = requests.post(url + "?lang=Kor", headers=headers, data=audio_data)
-
+    response = requests.post(url, headers=headers, data=audio_data)
     if response.status_code == 200:
         return response.json().get("text", "")
     else:
-        print("응답 오류:", response.status_code, response.text)
-        return ""
+        raise RuntimeError(f"STT 오류: {response.status_code} - {response.text}")
+
