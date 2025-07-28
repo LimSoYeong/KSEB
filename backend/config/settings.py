7a78676fd39451ab39cea6ed1dc306f25f946c67
jaeseong <wotjd0701@naver.com>
2025-07-28 17:22:20 +0900

    임시 저장

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -3,9 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-TTS_API_KEY = os.getenv("TTS_API_KEY")
-OPEN_API_URL = os.getenv("OPEN_API_URL")
-
-## 모든 API를 가져온다는 가정하에 API를 따로 관리(보안)
-## 학습좋은 모델있으면 API뺴고 routes,services,db에서 수정
+NAVER_CLIENT_ID = os.getenv("NAVER_CLIENT_ID")
+NAVER_CLIENT_SECRET = os.getenv("NAVER_CLIENT_SECRET")
+TTS_API_URL = os.getenv("TTS_API_URL")
+STT_API_URL = os.getenv("STT_API_URL")
