e75a2e39856db2c4979dd766d25266f3a4b4bfcc
jaeseong <wotjd0701@naver.com>
2025-07-28 13:50:06 +0900

    lang serve 리펙토링

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
-from routes import summarize, qa_router, chat_ws_router
 from langserve import add_routes
 from runnables.summarize_runnable import summarize_runnable
 from runnables.chat_runnable import chat_runnable
@@ -10,12 +9,15 @@ from runnables.stt_runnable import stt_runnable
 
 app = FastAPI()
 
-app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
-app.mount("/static", StaticFiles(directory="static"), name="static")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-app.include_router(summarize.router, prefix="/summarize")
-app.include_router(qa_router.router, prefix="/qa")
-app.include_router(chat_ws_router.router)
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 add_routes(app, summarize_runnable, path="/lang/summarize")
 add_routes(app, chat_runnable, path="/lang/chat")
