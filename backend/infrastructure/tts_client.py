da93bbc2ba816ecc3057d5a2f4bace0013b4adb4
LimSoYeong <soyeong020420@gmail.com>
2025-08-01 18:55:08 +0900

    fix: tts API호출시 필요없는 print 제거

--- backend/infrastructure/tts_client.py
+++ backend/infrastructure/tts_client.py
@@ -6,16 +6,13 @@ from config.settings import NAVER_CLIENT_ID, NAVER_CLIENT_SECRET, APP_SERVER_URL
 TTS_URL = "https://naveropenapi.apigw.ntruss.com/tts-premium/v1/tts"
 
 def text_to_speech(text: str) -> bytes:
-    print(f"[📏 TTS 요청 텍스트 길이] {len(text)}자")
+    print(f"[TTS 요청 텍스트 길이] {len(text)}자")
     filename = f"{uuid.uuid4().hex}.mp3"
     path = Path("static/audio") / filename
     try:
         path.parent.mkdir(parents=True, exist_ok=True) # 폴더 없을 경우 생성
     except FileExistsError:
         pass
-
-    print("[🔐 ID]", NAVER_CLIENT_ID)
-    print("[🔐 SECRET]", NAVER_CLIENT_SECRET)
 
     headers = {
         "x-ncp-apigw-api-key-id": NAVER_CLIENT_ID,
@@ -31,14 +28,7 @@ def text_to_speech(text: str) -> bytes:
 
     response = requests.post(TTS_URL, headers=headers, data=data)
     print("[TTS 요청]", data)
-    print("[TTS 응답 코드]", response.status_code)
-    print("[TTS 응답 내용]", response.content)
     if response.status_code == 200:
-
         return response.content
-
-        # with open(path, "wb") as f:
-        #     f.write(response.content)
-        # return f"{APP_SERVER_URL}/static/audio/{filename}" # 클라이언트용 경로
     else:
         raise RuntimeError(f"TTS 요청 실패: {response.status_code} - {response.text}")
